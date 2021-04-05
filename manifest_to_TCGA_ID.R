a5a13db017c1fcbee6d6cfc7dc836c3d15cb358a
Pamela M <pamelamishra@gmail.com>
2021-04-05 15:40:23 -0400

    Update manifest_to_TCGA_ID.R

--- manifest_to_TCGA_ID.R
+++ manifest_to_TCGA_ID.R
@@ -1,11 +1,18 @@
+
+#!/usr/bin/env Rscript
+
+
 #Info frm 
 #https://www.bioconductor.org/packages/release/bioc/vignettes/TCGAutils/inst/doc/UNC12-SN629_0322.html#uuid-to-tcga-barcode
 library(TCGAutils)
 
-manifest <- read.table("", header=TRUE, sep="\t")
+#Accepts 1 argument which is the name of the manifest file(Sorry no checks)
+args = commandArgs(trailingOnly=TRUE)
+
+manifest <- read.table(args[1], header=TRUE, sep="\t")
 file_uuids <- manifest$id
 
-for (i in  file_uuids){
-        result = UUIDtoBarcode(i, id_type = "file_id")
+for (f in  file_uuids){
+        result = UUIDtoBarcode(f, id_type = "file_id")
         print (paste(result$file_id, result$cases.submitter_id , sep = " "))
 }
